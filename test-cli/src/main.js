dc820ea18fe634820bde001ad3810c2dec32c2c0
Liberbell <liberbell@gmail.com>
2020-08-29 17:45:59 +0900

    main1

--- test-cli/src/main.js
+++ test-cli/src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
+var app = new Vue({
+  el: '#the_food_app',
+  data: {
+      title: 'The Food App',
+      subtitle: 'This app is about food!',
+  }
+});
