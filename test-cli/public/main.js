5e5d24310889a973c605ea5a04640e248df4b1f9
Liberbell <liberbell@gmail.com>
2020-08-29 17:44:36 +0900

    main4

--- test-cli/public/main.js
+++ test-cli/public/main.js
@@ -2,6 +2,6 @@ var app = new Vue({
     el: '#the_food_app',
     data: {
         title: 'The Food App',
-        subtitle: "This app is about food!",
+        subtitle: 'This app is about food!',
     }
 });
