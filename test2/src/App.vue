68739df6530e047eb4eaf60324964e74aa417578
Liberbell <liberbell@gmail.com>
2020-09-07 09:00:10 +0900

    app1

--- test2/src/App.vue
+++ test2/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js by liber"/>
-    <HelloWorld text="Test data"/>
+    <!-- <HelloWorld text="Test data"/> -->
   </div>
 </template>
 
