9938707347611a50e04d3e5c542f43d2e7091fdc
Liberbell <liberbell@gmail.com>
2020-09-08 09:06:00 +0900

    helloworld14

--- test2/src/components/HelloWorld.vue
+++ test2/src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2 v-show="showText">{{ text }}</h2>
     <button v-on:click="toggle">toggle</button>
+    <h2 v-show="showText">{{ text }}</h2>
   </div>
 </template>
 
