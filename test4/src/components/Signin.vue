22b2f633e6378d7e88e7999795180c912aa7f557
Liberbell <liberbell@gmail.com>
2020-09-10 21:44:33 +0900

    signin2

---
+++ test4/src/components/Signin.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="signin">
+    <h1>サインイン</h1>
+    <input type="text" placeholder="ユーザ名" v-model="username">
+    <input type="password" placeholder="パスワード" v-model="password">
+    <button>サインイン</button>
+    <p>アカウント登録はしましたか? <br>
+      <router-link to="/signup">ここをクリックしてアカウント登録をしてください</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Signin',
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  }
+}
+</script>
