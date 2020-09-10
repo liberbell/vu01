df7c97bb9ba9654188e934b73d7c15355dd7c3be
Liberbell <liberbell@gmail.com>
2020-09-10 21:46:48 +0900

    success2

---
+++ test4/src/components/Success.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="success">
+    <h1>サインイン成功</h1>
+    <h2>ようこそアドレス名:{{ '' }}さん</h2>
+   <button @click="signOut">サインアウト</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'success',
+  data () {
+      return ''
+  }
+  </script>
