5abfcc09776ad7ea3c0a1de069f535a8f328f2ae
Liberbell <liberbell@gmail.com>
2020-09-10 21:43:28 +0900

    signup2

---
+++ test4/src/components/Signup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="signup">
+    <h1>アカウント登録</h1>
+    <input type="text" placeholder="ユーザー名" v-model="username">
+    <input type="password" placeholder="パスワード" v-model="password">
+    <button>登録</button>
+    <p>
+      <router-link to="/signin">サインインはこちらからできます</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Signup',
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  }
+}
+</script>
